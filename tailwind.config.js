ca94136ab01f1032ad0672c2c7cb677eef188e70
[Aashish Rai] <[2aashishrai@gmail.com]>
2022-09-18 13:19:25 +0545

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        600: "#ce7924",
+      },
+      "light-grey": "#f5f0eb",
+      white: "#ffffff",
+      content: {
+        DEFAULT: "#696158",
+        placeholder: " rgba(95, 95, 95, 0.8)",
+      },
+      line: {
+        DEFAULT: "#aaaaaa",
+        focus: "#696158",
+        disabled: "#efefef",
+      },
+      error: {
+        base: "#f81e1e",
+      },
+    },
+    extend: {
+      spacing: {
+        7.5: "1.875rem", //30px
+        25: "6.25rem", //100px
+      },
+      boxShadow: {
+        primary: " 0 1.25rem 2.5rem 0 rgba(105, 97, 88, 0.2)",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
